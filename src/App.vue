2a94409743d2879c3838538419028d3e6e278c00
maoshimin <1139630078@qq.com>
2025-11-16 09:48:53 +0800

    初始化cesium地球

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
-<script setup lang="ts"></script>
+
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+<div id="cesiumContainer"></div>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { Viewer,Ion ,SceneMode} from 'cesium';
+
+Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3N2QzMDM3Yy1lNTcwLTQ5YTktODg0Yy01ZDg4NzIwYjQxNTkiLCJpZCI6MzYwNTU1LCJpYXQiOjE3NjMxNzc0NTZ9.0efxBLC2furS_N97Z3_oM7KN-pK29ek7tLil1rNWrTE';
+onMounted(() => {
+  window._viewer = new Viewer('cesiumContainer', {
+    sceneMode: SceneMode.SCENE3D,
+    animation: false,
+    baseLayerPicker: false,
+    geocoder: false,
+    timeline: false,
+    navigationHelpButton: false,
+
+  });
+});
+</script>
+
+<style scoped>
+#cesiumContainer {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  overflow: hidden;
+}
+</style>
